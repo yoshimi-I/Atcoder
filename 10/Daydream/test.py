0383317c5477c4681eb018d86f05beff4760eff8
Yoshimi-I <89241539+yoshimi-I@users.noreply.github.com>
2021-12-22 12:49:14 +0900

    add: 問9の追加

---
+++ 10/Daydream/test.py
@@ -0,0 +1,28 @@
+# S = input()
+
+# while len(S) >= 5:
+#     if len(S) >= 7 and S[-7:] == "dreamer":
+#         S = S[:-7]
+#         continue
+
+#     if len(S) >= 6 and S[-6:] == "eraser":
+#         S = S[:-6]
+#         continue
+
+#     elif S[-5:] == "dream" or S[-5:] == "erase":
+#         S = S[:-5]
+#         continue
+
+#     else:
+#         break
+
+# if len(S) == 0:
+#     print("YES")
+# else:
+#     print("NO")
+
+A = "こんにちは僕の名前は岩月ですよろしくお願いします。"
+print(A[:5])
+print(A[14:])
+print(A[-7:])
+print(A[:-11])
