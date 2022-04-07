ee0735c9169ae400a51411600b35b241fd8236e9
yoshimi-I <iwatsuki.443@outlook.com>
2022-04-07 14:09:17 +0900

    add: 解答の追加

--- 50/22/try.py
+++ 50/22/try.py
@@ -1,4 +1,24 @@
 N,K= map(int,input().split())
 p = list(map(int,input().split()))
-def kitaiti(num):
-    return 
+ans = 0
+def kitaichi(num):
+    add = num
+    count = 0
+    while add > 0:
+        count += add
+        add -= 1
+    return float(count)/float(num)
+L = []
+for i in range(N):
+    if i == 0:
+        L.append(kitaichi(p[i]))
+    else:
+        L.append(kitaichi(p[i]) + L[i-1])
+for j in range(K-1,N):
+    if j == K-1:
+        check = L[j]
+    else:
+        check = L[j]-L[j-K]
+    if check >= ans:
+        ans = check
+print(ans)
