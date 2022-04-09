ffa1df85524a97f6eb8a90e80caa907c189f69c1
yoshimi-I <iwatsuki.443@outlook.com>
2022-04-10 00:17:21 +0900

    add: 解答の追加

--- 50/26/try.py
+++ 50/26/try.py
@@ -1,3 +1,23 @@
 from collections import deque
-S = list(input())
-A = deque
+S = deque(input())
+swich_num = 1
+Q = int(input())
+for _ in range(Q):
+    L = list(input().split())
+    if L[0] == "1":
+        swich_num*=(-1)
+    if L[0] == "2":
+        if L[1] == "1":
+            if swich_num > 0:
+                S.appendleft(L[2])
+            else:
+                S.append(L[2])
+        else:
+            if swich_num > 0:
+                S.append(L[2])
+            else:
+                S.appendleft(L[2])
+if swich_num < 0:
+    S.reverse()
+ans = "".join(S)
+print(ans)
