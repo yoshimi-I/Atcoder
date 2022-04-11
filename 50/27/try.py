f005355afeaef283a525c52f5b4862773f7a7d62
yoshimi-I <iwatsuki.443@outlook.com>
2022-04-11 17:49:49 +0900

    fix: mdへのルートの変更に基づく修正

--- 50/27/try.py
+++ 50/27/try.py
@@ -1,8 +1,12 @@
+from collections import deque
+
+
 N,M = map(int,input().split())
 P = [[] for _ in range(N+1)]
 for _ in range(M):
     A,B = map(int,input().split())
     P[A].append(B)
+que = deque()
 
 
 
